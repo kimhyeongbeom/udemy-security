81164df05d1a53c12ea83e288669827b9032dd48
leopard2 <ncbomb@naver.com>
2024-05-01 07:13:17 +0900

    3장 소스

---
+++ springsecsecurity3/src/test/java/com/easybytes/springsecsecurity2/EasyBankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easybytes.springsecsecurity2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyBankBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
