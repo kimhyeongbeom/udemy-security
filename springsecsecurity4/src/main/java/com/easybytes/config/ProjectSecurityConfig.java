4ccfd2bd4229ec7edba6b66e162c7167456b696c
hbkim <kimhyeongbeom@gmail.com>
2024-05-03 21:58:05 +0900

    4강소스 시작

---
+++ springsecsecurity4/src/main/java/com/easybytes/config/ProjectSecurityConfig.java
@@ -0,0 +1,64 @@
+package com.easybytes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/myAccount", "/myBalance", "/myLoans", "myCards").authenticated()
+                .requestMatchers("/contact", "/notices", "/register").permitAll() )
+        .formLogin(withDefaults())
+        .httpBasic(withDefaults());
+        return http.build();
+    }
+
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService() {
+///*
+//        UserDetails admin = User.withDefaultPasswordEncoder()
+//                .username("admin")
+//                .password("12345")
+//                .roles("ADMIN")
+//                .build();
+//
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("12345")
+//                .roles("READ")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(admin, user); */
+//
+////        UserDetails admin = User.withUsername("admin").password("12345").roles("ADMIN").build();
+////        UserDetails user = User.withUsername("user").password("12345").roles("READ").build();
+////        return new InMemoryUserDetailsManager(admin, user);
+//    }
+//
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource) {
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
