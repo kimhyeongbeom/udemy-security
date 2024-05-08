ae0f4592b9763f9eca157021abac50811867e51f
hbkim <kimhyeongbeom@gmail.com>
2024-05-08 17:06:15 +0900

    7장소스 추가

---
+++ springsecsecurity7/src/main/java/com/eazybytes/config/EazyBankUsernamePwdAuthenticationProvider.java
@@ -0,0 +1,49 @@
+package com.eazybytes.config;
+
+import com.eazybytes.model.Customer;
+import com.eazybytes.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class EazyBankUsernamePwdAuthenticationProvider implements AuthenticationProvider {
+
+    private final CustomerRepository customerRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        List<Customer> customers = customerRepository.findByEmail(username);
+        if (!customers.isEmpty()) {
+            Customer customer = customers.get(0);
+            if (passwordEncoder.matches(password, customer.getPwd())) {
+                List<GrantedAuthority> authorities = new ArrayList<>();
+                authorities.add(new SimpleGrantedAuthority(customer.getRole()));
+                return new UsernamePasswordAuthenticationToken(username, password, authorities);
+            } else {
+                throw new BadCredentialsException("Password does not match");
+            }
+        } else {
+            throw new BadCredentialsException("User not found : " + username);
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+}
