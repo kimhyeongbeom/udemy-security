b65b7110ab3c0d5d5dc0dc1457c219933ab7dce1
leopard2 <ncbomb@naver.com>
2024-05-12 08:37:55 +0900

    10장 소스추가

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -9,3 +9,9 @@ J
 springsecsecurity9/pom.xml,3\5\35ab78bd9f02b1d3348f822c098fba199942b994
 l
 <springsecsecurity9/src/main/resources/application.properties,1\0\10aff271f634365f657828cbe93562ef597263c9
+€
+Pspringsecsecurity9/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java,a\0\a0131400b3cef7c3c10507e75fcf900ba5f520e8
+‚
+Rspringsecsecurity9/src/main/java/com/eazybytes/filter/JWTTokenGeneratorFilter.java,a\8\a8926df4fa82ebbb3b32e50aa98c30730b221e05
+‚
+Rspringsecsecurity9/src/main/java/com/eazybytes/filter/JWTTokenValidatorFilter.java,a\9\a92ba3ca686b1261b58aee8376020bc673a7ddf4
---
+++ springsecsecurity10/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -0,0 +1,62 @@
+package com.eazybytes.config;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import com.eazybytes.filter.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.web.cors.CorsConfiguration;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        CsrfTokenRequestAttributeHandler csrfTokenHandler = new CsrfTokenRequestAttributeHandler();
+        csrfTokenHandler.setCsrfRequestAttributeName("_csrf");
+
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) )
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(request -> {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                    config.setAllowedMethods(Collections.singletonList("*"));
+                    config.setAllowCredentials(true);
+                    config.setAllowedHeaders(Collections.singletonList("*"));
+                    config.setExposedHeaders(Arrays.asList("Authorization"));
+                    config.setMaxAge(3600L);
+                    return config;
+                }))
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/register", "/contact"))
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+                .addFilterBefore(new RequestValidationBeforeFilter(), BasicAuthenticationFilter.class)
+                .addFilterAfter(new AuthoritiesLoggingAfterFilter(), BasicAuthenticationFilter.class)
+                .addFilterAt(new AuthoritiesLoggingAtFilter(), BasicAuthenticationFilter.class)
+                .addFilterAfter(new JWTTokenGeneratorFilter(), AuthoritiesLoggingAtFilter.class)
+                .addFilterBefore(new JWTTokenValidatorFilter(), AuthoritiesLoggingAtFilter.class)
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/myAccount").hasRole("USER")
+                        .requestMatchers("/myBalance").hasAnyRole("USER","ADMIN")
+                        .requestMatchers("/myLoans").hasRole("USER")
+                        .requestMatchers("/myCards").hasRole("USER")
+                        .requestMatchers("/user").authenticated()
+                        .requestMatchers("/notices", "/contact", "/register").permitAll())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
