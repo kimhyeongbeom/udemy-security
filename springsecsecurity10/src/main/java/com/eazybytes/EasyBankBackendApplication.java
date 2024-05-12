d1d16fd5d0de988812a04028b0bc6078d972397e
leopard2 <ncbomb@naver.com>
2024-05-12 13:52:24 +0900

    10장 소스

--- springsecsecurity10/src/main/java/com/eazybytes/EasyBankBackendApplication.java
+++ springsecsecurity10/src/main/java/com/eazybytes/EasyBankBackendApplication.java
@@ -2,10 +2,11 @@ package com.eazybytes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-@EnableWebSecurity(debug = true)
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class EasyBankBackendApplication {
 
     public static void main(String[] args) {
