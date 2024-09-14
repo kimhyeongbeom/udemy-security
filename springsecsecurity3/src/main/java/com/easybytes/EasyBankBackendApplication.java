cdc6d045de889b50d01c6486b7357e67ed62601d
leopard2 <ncbomb@naver.com>
2024-09-14 13:58:57 +0900

    33강 수강완료

--- springsecsecurity3/src/main/java/com/easybytes/EasyBankBackendApplication.java
+++ springsecsecurity3/src/main/java/com/easybytes/EasyBankBackendApplication.java
@@ -2,8 +2,14 @@ package com.easybytes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+//@EnableJpaRepositories("com.easybytes.repository")
+//@EntityScan("com.easybytes.model")
+@EnableWebSecurity
 public class EasyBankBackendApplication {
 
     public static void main(String[] args) {
--- springsecsecurity3/src/main/java/com/easybytes/model/Customer.java
+++ springsecsecurity3/src/main/java/com/easybytes/model/Customer.java
@@ -1,19 +1,18 @@
-//package com.easybytes.model;
-//
-//import org.hibernate.annotations.GenericGenerator;
-//import jakarta.persistence.*;
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//@Entity
-//@Getter
-//@Setter
-//public class Customer {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
-//    @GenericGenerator(name = "native", strategy = "native")
-//    private Long id;
-//    private String email;
-//    private String pwd;
-//    private String role;
-//}
+package com.easybytes.model;
+
+import org.hibernate.annotations.GenericGenerator;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String email;
+    private String pwd;
+    private String role;
+}
