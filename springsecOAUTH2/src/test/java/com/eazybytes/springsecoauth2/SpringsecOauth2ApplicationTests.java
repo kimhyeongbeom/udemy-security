117b40023ca8322afbbd6ca297d7fa42e53e3107
leopard2 <ncbomb@naver.com>
2024-10-06 22:42:00 +0900

    130강 수강중

---
+++ springsecOAUTH2/src/test/java/com/eazybytes/springsecoauth2/SpringsecOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybytes.springsecoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
